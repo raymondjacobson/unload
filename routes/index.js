0da7468a691463a4c38aa9f578794e47cbef9a5b
MoDaum <daum@umich.edu>
2014-06-28 15:34:33 -0700

    function is included in get_locations call

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var location_methods = require('../lib/location_methods.js');
 
 // route for incoming ios msg service
 exports.in_ios = function(req, res) {
-  res.send(location_methods.get_locations('metal'));
+  res.send(location_methods.get_locations('metal', location_methods.get_info_sheet));
 }
 
 
