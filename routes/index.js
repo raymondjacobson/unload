4e6de3197794597b99be7fb790d41bc72bf92b2e
MoDaum <daum@umich.edu>
2014-06-28 16:00:45 -0700

    find all locations for recycling material and info sheet, if available

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var location_methods = require('../lib/location_methods.js');
 
 // route for incoming ios msg service
 exports.in_ios = function(req, res) {
-  res.send(location_methods.get_locations('metal', location_methods.get_info_sheet));
+  res.send(location_methods.get_locations('metal', location_methods.get_info_sheet, location_methods.get_greeenwaste_recyclers));
 }
 
 
